dc50368d5c0fc845e6288059ab046ecfa247a969
unknown <hoyoooo@gmail.com>
2018-12-04 15:52:40 +0300

    @MariamAlSubaiei

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import authors from "./data.js";
+//import authors from "./data.js";
 
 // Components
 import Sidebar from "./Sidebar";
 import SearchBar from "./SearchBar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+//import Axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentAuthor: {},
-      filteredAuthors: []
+      filteredAuthors: [],
+      authors: []
     };
     this.selectAuthor = this.selectAuthor.bind(this);
     this.unselectAuthor = this.unselectAuthor.bind(this);
@@ -31,28 +33,48 @@ class App extends Component {
 
   filterAuthors(query) {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`.includes(query);
     });
     this.setState({ filteredAuthors: filteredAuthors });
   }
 
+  componentDidMount() {
+    console.log("did mount");
+    //this.setState({ authors: authors });
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(res => res.data)
+      .then(authorsList => this.setState({ authors: authorsList }));
+
+  }
+
   getContentView() {
+
     if (this.state.currentAuthor.first_name) {
+
       return <AuthorDetail author={this.state.currentAuthor} />;
-    } else if (this.state.filteredAuthors[0]) {
-      return (
-        <AuthorsList
-          authors={this.state.filteredAuthors}
-          selectAuthor={this.selectAuthor}
-        />
-      );
+
     } else {
-      return <AuthorsList authors={authors} selectAuthor={this.selectAuthor} />;
+
+      return (
+
+        <AuthorsList
+
+          authors={this.state.authors}
+
+          selectAuthor={this.selectAuthor}
+
+        />
+
+      );
+
     }
+
   }
 
   render() {
+    console.log("render");
     return (
       <div id="app" className="container-fluid">
         <div className="row">
